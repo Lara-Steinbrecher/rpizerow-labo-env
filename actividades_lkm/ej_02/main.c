1dd8a4593b40c562a837c50d88ab2875947153e9
ValentinoGonzalezPautaso2 <gonzalezpautasovalentino@gmail.com>
2024-05-02 21:11:25 +0100

    feat: ejercicio 02 main.c

--- actividades_lkm/ej_02/main.c
+++ actividades_lkm/ej_02/main.c
@@ -7,18 +7,51 @@
 #include <linux/sched/signal.h>
 
 // Variables globales para kernel threads
-static struct task_struct *kthread_1 = NULL;
-static struct task_struct *kthread_2 = NULL;
+static struct task_struct *kthread1 = NULL;
+static struct task_struct *kthread2 = NULL;
 
 // TODO:
 // Funciones para los hilos
+static int hilo1(void *params) {
+	// Creacion de variables
 
+	// Bucle mientras que la tarea no sea detenida
+	while(!kthread_should_stop()) {
+		// Tareas recurrentes
+	printk("Steinbrecher_Lara_ej02: Hola desde el primer hilo :D");
+	msleep(100);
+	}
+	// Finalizacion del hilo
+	return 0;
+}
+static int hilo2(void *params) {
+	// Creacion de variables
+
+	// Bucle mientras que la tarea no sea detenida
+	while(!kthread_should_stop()) {
+		// Tareas recurrentes
+	printk("Steinbrecher_Lara_ej02: Hola desde el segundo hilo D:");
+	msleep(100);
+	}
+	// Finalizacion del hilo
+	return 0;
+}
 /**
  * @brief Se llama cuando se carga en el kernel
 */
 static int __init ej02_module_init(void) {
 	// TODO:
 	// - Crear ambos hilos
+	kthread1 = kthread_create(hilo1,NULL,"kthread1");
+	if (kthread1 == NULL){
+	printk("Steinbrecher_Lara_ej02: error no se pudo crear hilo 1");
+}
+	wake_up_process(kthread1);
+	kthread2 = kthread_create(hilo2,NULL,"kthread2");
+	if(kthread2 == NULL){
+	printk ("Steinbrecher_Lara_ej02: error no se pudo crear hilo 2");
+}
+	wake_up_process(kthread2);
 	// - Verificar que se hayan podido crear
 	// - Correr hilos
 	return 0;
@@ -30,6 +63,8 @@ static int __init ej02_module_init(void) {
 static void __exit ej02_module_exit(void) {
 	// TODO:
 	// - Detener hilos
+	kthread_stop(kthread1);
+	kthread_stop(kthread2);
 }
 
 // Registro funciones de inicializacion y salida
@@ -38,5 +73,5 @@ module_exit(ej02_module_exit);
 
 // Informacion del modulo (completar lo que corresponda)
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("");
-MODULE_DESCRIPTION("");
+MODULE_AUTHOR("Steinbrecher_Lara");
+MODULE_DESCRIPTION("Creamos 2 hilos y corre uno luego el otro asi hasta que el programa se detenga");
